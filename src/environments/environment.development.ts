e923ac13d132e553db5625a40070cf6ae242b1d9
nicoonievas <nicolasnievas1@gmail.com>
2024-11-14 19:09:42 -0300

    test var.entorno

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: true,
   API_URL: 'https://api.github.com',  // Define el valor por defecto para desarrollo
   API_KEY: ''                  // Define el valor por defecto para desarrollo
+
+  
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: false,
   API_URL: 'https://api.github.com',  // Define el valor por defecto para desarrollo
   API_KEY: ''                  // Define el valor por defecto para desarrollo
+
+  
 };
