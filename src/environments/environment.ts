5c81191fe6231bfe2948a39a732a60c2700a80df
Gino <gino1v@hotmail.com>
2024-11-13 22:46:46 -0300

    aa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://default-api-url.comaaaa',  // Valor por defecto
-  apiKey: 'default-api-keyaaaaaaaaa'  // Valor por defecto
+  apiUrl: process.env['API_URL'] || 'hdddd',  // Usar corchetes
+  apiKey: process.env['API_KEY'] || 'aaa'  // Usar corchetes
 };
