09e02577c2d9c18ab405fb6b4760e922606b8eda
Gino <gino1v@hotmail.com>
2024-11-14 17:11:38 -0300

    aaaa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   API_URL: 'https://api.github.com',  // Define el valor por defecto para desarrollo
-  API_KEY: 'aaa'                  // Define el valor por defecto para desarrollo
+  API_KEY: 'aa'                  // Define el valor por defecto para desarrollo
 };
