ea9e176a66b00032d65f3397d23cb68438990542
Gino <gino1v@hotmail.com>
2024-11-14 15:46:01 -0300

    aaa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  API_URL: process.env['API_URL'] || 'https://api.github.com',  // Valor predeterminado si no se encuentra la variable
-  API_KEY: process.env['API_KEY'] || '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'  // Valor predeterminado si no se encuentra la variable
+  API_URL: 'https://api.github.com',  // Define el valor por defecto para desarrollo
+  API_KEY: 'aa'                  // Define el valor por defecto para desarrollo
 };
-
