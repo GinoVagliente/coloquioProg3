293337731138fd4af8e6bea1121c9be21fb120f8
GinoVagliente <143140690+GinoVagliente@users.noreply.github.com>
2024-11-14 18:36:52 -0300

    Delete src/environments/environment.ts

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  API_URL: 'https://api.github.com',  // Define el valor por defecto para desarrollo
-  API_KEY: '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'                  // Define el valor por defecto para desarrollo
-};
