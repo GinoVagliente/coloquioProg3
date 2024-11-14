8ce2a7e8b6f6ab5eb53840c77015d7c7310acb8e
Gino <gino1v@hotmail.com>
2024-11-13 22:36:38 -0300

    aa

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['API_URL'] || 'https://default-api-url.com',  // Valor predeterminado si no se encuentra la variable
-  apiKey: process.env['API_KEY'] || 'default-api-key'  // Valor predeterminado si no se encuentra la variable
+  apiUrl: process.env['API_URL'] || 'https://default-api-url.com',  // Usar corchetes
+  apiKey: process.env['API_KEY'] || 'default-api-key'  // Usar corchetes
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: process.env['API_URL'] || 'https://default-api-url.com',  // Valor predeterminado si no se encuentra la variable
-  apiKey: process.env['API_KEY'] || 'default-api-key'  // Valor predeterminado si no se encuentra la variable
+  apiUrl: 'https://default-api-url.com',  // Valor por defecto
+  apiKey: 'default-api-key'  // Valor por defecto
 };
