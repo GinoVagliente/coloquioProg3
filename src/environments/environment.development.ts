cdd8f20038a5327f96b4193aee6b13234bca8c03
Gino <gino1v@hotmail.com>
2024-11-13 22:44:19 -0300

    aa

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['API_URL'] || 'https://default-api-url.com',  // Usar corchetes
-  apiKey: process.env['API_KEY'] || 'default-api-key'  // Usar corchetes
+  apiUrl: process.env['API_URL'] || 'https://github.com',  // Usar corchetes
+  apiKey: process.env['API_KEY'] || 'https://api.github.com'  // Usar corchetes
 };
