0201b74842e7e0128cc31909a99782803ec2daf3
GinoVagliente <143140690+GinoVagliente@users.noreply.github.com>
2024-11-14 18:22:08 -0300

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  API_URL: 'https://api.github.com',  // Define el valor por defecto para desarrollo
+  API_KEY: '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'                  // Define el valor por defecto para desarrollo
+};
